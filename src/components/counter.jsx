98bc7fc36ac8fc0d0286caf7687ba118092e569d
SMFahimShahrear <s.m.shahrear.fahim@gmail.com>
2022-05-17 12:36:58 +0600

    coumt system desigend

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    render() { 
-        return <h1>Hello World!</h1>;
+    state = {
+        count: 0
+    };
+
+    render() {
+        return (
+        <React.Fragment>
+            <span>{this.formetCount()}</span>
+            <button>Increament</button>
+        </React.Fragment>
+        );
+    }
+
+    formetCount()
+    {
+        const {count} = this.state;
+        return count === 0? 'zero' : count;
     }
 }
  
 export default Counter;
+
+
+//------------40.48-----------
